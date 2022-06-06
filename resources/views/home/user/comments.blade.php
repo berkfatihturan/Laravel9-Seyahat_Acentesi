45d047e12e32edf4296f5e09eade7189e7149423
berkfatihturan <bft_@outlook.com>
2022-06-06 19:03:46 +0300

    Proje Updated

--- resources/views/home/user/comments.blade.php
+++ resources/views/home/user/comments.blade.php
@@ -12,6 +12,9 @@
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 @endsection
 
 @section('nagivation')
@@ -26,8 +29,9 @@
             <div class="col-md-2 user-nav">
                 @include('home.user.usermenu')
             </div>
-            <div class="col-md-10 user-text">
+            <div class="col-md-10 user-text" id="comments">
                 <h3 style="margin-bottom: 40px;margin-top: 20px;">Your Comments</h3>
+                <label style="margin-left: 10px @if($info=="") display:none @endif">{{\Illuminate\Support\Facades\Session::get('info')}}</label>
                 @foreach($comments as $cmt)
 
                     <div class="col-md-12 mb-2">
@@ -61,7 +65,7 @@
                                         </a>
                                     </li>
                                     <li class="list-inline-item g-mr-20">
-                                        <a class="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover" href="#!">
+                                        <a href="#comment-update-form-{{$cmt->id}}" data-toggle="collapse"  class="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover">
                                             <i class="fa-solid fa-message"></i>
                                             Update
                                         </a>
@@ -71,12 +75,47 @@
                                             Status : {{$cmt->status}}
                                         </a>
                                     </li>
-
                                 </ul>
 
                             </div>
                         </div>
                     </div>
+
+                    <div id="comment-update-form-{{$cmt->id}}" class="collapse col-xs-11 col-sm-11 float-right ">
+
+                        <form id="checkout-form" class="form-block" style="width: 100%; margin-bottom: 10px;" method="post" action="{{route('userpanel_commentUpdate',['id'=>$cmt->id])}}">
+                            @csrf
+                            <div class="col-xs-12 col-sm-12">
+                                <div class="form-group fl_icon">
+                                    <input class="form-input" name="subject" type="text" placeholder="Subject" value="{{$cmt->subject}}">
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12">
+                                <div class="form-group">
+                                    <textarea name="comment" class="form-input" placeholder="Your Comment">{{$cmt->comment}}
+                                    </textarea>
+                                </div>
+                            </div>
+
+                            <div class="col-xs-12">
+                                <div class="form-group">
+                                    <label class="col-md-3 control-label" style="width: auto">YOUR RATING:</label>
+                                    <div class="rating">
+                                        <input type="radio" name="rate" value="5" id="5-{{$cmt->id}}" @if($cmt->rate==5)  checked @endif><label for="5-{{$cmt->id}}">☆</label>
+                                        <input type="radio" name="rate" value="4" id="4-{{$cmt->id}} " @if($cmt->rate==4)  checked @endif><label for="4-{{$cmt->id}}">☆</label>
+                                        <input type="radio" name="rate" value="3" id="3-{{$cmt->id}}" @if($cmt->rate==3)  checked @endif><label for="3-{{$cmt->id}}">☆</label>
+                                        <input type="radio" name="rate" value="2" id="2-{{$cmt->id}}" @if($cmt->rate==2)  checked @endif><label for="2-{{$cmt->id}}">☆</label>
+                                        <input type="radio" name="rate" value="1" id="1-{{$cmt->id}}" ><label for="1">☆</label>
+                                    </div>
+                                    <input type="hidden" name="package_id" value="">
+                                    <button class="btn btn-primary pull-right" style="border-radius: 5px; font-weight:500">Update</button>
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
+
                 @endforeach
             </div>
         </div>
